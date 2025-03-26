37e66763b3e916aaa6df21e1624e82b3c0dce0f1
Jean2505 <jean.victor12357@gmail.com>
2025-03-26 19:54:18 -0300

    bindings de evento do login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  @Output() submit = new EventEmitter<void>();
+  enteredEmail = '';
+  enteredPassword = '';
 
+  onSubmit() {
+    this.submit.emit();
+  }
 }
