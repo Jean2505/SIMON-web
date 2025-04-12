91e068c272b7493ef33f3d8da568ab102e48e0f4
João <joao.ferraz.balducci@gmail.com>
2025-04-12 01:33:15 -0300

    added the createCourses cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,11 +9,67 @@
 
 import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
+import * as admin from "firebase-admin";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-// export const helloWorld = onRequest((request, response) => {
+const app = admin.initializeApp();
+const db = app.firestore();
+
+interface CallableResponse{
+    status: string,
+    message: string,
+    payload: string
+  }
+
+interface Course {
+    name: string;
+    professor: string;
+    term: string;
+    id: number;
+    school: string;
+    course: string;
+    monitors: number;
+}
+
+// export const helloWorld = onRequest((req, res) => {
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+export const createCourses = onRequest({region: "southamerica-east1"}, (req, res) => {
+    let result: CallableResponse;
+    
+    logger.debug(req.body)
+
+    try {
+        req.body.courses.forEach(async (course: any) => {
+            logger.debug(course)
+            let coursefb: Course = {
+                name: course.nome_Disciplina,
+                professor: course.professor_Disciplina,
+                term: course.periodo_Disciplina,
+                id: course.id_Disciplina,
+                school: course.escola_Disciplina,
+                course: course.curso_Disciplina,
+                monitors: 0
+            }
+            await db.collection("Courses").add(coursefb);
+        })
+        result = {
+            status: "OK",
+            message: "Courses created successfully",
+            payload: "Courses created successfully"
+        };
+        res.status(200).send(result);
+    } catch (error) {
+        logger.error(error)
+        result = {
+            status: "ERROR",
+            message: "Error creating courses",
+            payload: (error as Error).message
+        };
+        res.status(500).send(result);
+    }
+});
