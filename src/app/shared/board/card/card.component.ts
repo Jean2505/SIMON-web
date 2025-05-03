99e76f7a93f40c8632a14a3c9fffd1f98d7eeac3
Marcos <marcos.c.junior2@gmail.com>
2025-05-03 12:41:39 -0300

    Mural

--- src/app/shared/board/card/card.component.ts
+++ src/app/shared/board/card/card.component.ts
@@ -1,18 +1,29 @@
 import { Component, Input } from '@angular/core';
-import { DatePipe } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 
 import { type Post } from '../../../models/post.model';
 import { PostComponent } from "../post/post.component";
 
 @Component({
   selector: 'app-card',
-  imports: [DatePipe, PostComponent],
+  imports: [DatePipe, PostComponent, CommonModule ],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
   @Input() post?: Post; 
 
+  color: string = '';
+
+  ngOnInit(): void {
+    this.color = this.getRandomColor();
+  }
+
+  private getRandomColor(): string {
+    const colors = ['#f44336', '#2196f3', '#4caf50', '#ff9800', '#9c27b0'];
+    return colors[Math.floor(Math.random() * colors.length)];
+  }
+  
   isViewingPost = false;
 
   onOpenPost(){
