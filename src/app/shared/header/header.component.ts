656bad3e6ef02f09ea42341787041d5ba9ed181b
Luan Magri Tarran <luan.mtarran@gmail.com>
2025-05-19 14:47:59 -0300

    Alterações de header e rotas

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { Auth, signOut } from '@angular/fire/auth';
 
 @Component({
@@ -9,16 +9,56 @@ import { Auth, signOut } from '@angular/fire/auth';
   templateUrl: './header.component.html', // Caminho para o template HTML
   styleUrls: ['./header.component.scss']  // Caminho para o(s) stylesheet(s)
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  /**
+   * Variável para armazenar o nome do usuário logado.
+   * @type {string}
+   */
+  nome!: string;
+
   /**
    * Construtor do componente.
    * @param router - Injeção do serviço de roteamento do Angular para navegação programática.
    * @param auth   - Injeção do serviço de autenticação do Firebase.
    */
   constructor(
+    /** Injeção do serviço de roteamento do Angular @type {Router} */
     private router: Router,
-    private auth: Auth
+    /** Injeção do serviço de autenticação do Firebase @type {Auth} */
+    private auth: Auth,
+    /** Referência ao serviço de rota ativa @type {ActivatedRoute} */
+    private route: ActivatedRoute
   ) { }
+
+  /**
+   * Método de inicialização do componente.
+   * Aqui você pode adicionar qualquer lógica que precise ser executada quando o componente é criado.
+   */
+  ngOnInit(): void {
+    // Coloca o nome do usuário logado no console
+    this.nome = this.auth.currentUser?.displayName!;
+  }
+
+  /**
+   * Método para navegar para a tela inicial da instituição.
+   * Usa o Router.navigate e registra no console se a navegação foi bem-sucedida ou não.
+   */
+  goHome(): void {
+    this.router.navigate(['home'], { relativeTo: this.route.pathFromRoot[1] })
+      .then(success => console.log('Navegação realizada:', success))
+      .catch(error => console.error('Erro na navegação:', error));
+  }
+
+  /**
+   * Método para navegar para a tela de gerenciamento de perfil.
+   * Usa o Router.navigate e registra no console se a navegação foi bem-sucedida ou não.
+   */
+  goManageProfile(): void {
+    this.router.navigate(['manage-profile'], { relativeTo: this.route.pathFromRoot[1] })
+      .then(success => console.log('Navegação realizada:', success))
+      .catch(error => console.error('Erro na navegação:', error));
+  }
 
   /**
    * Método de logout.
@@ -36,15 +76,5 @@ export class HeaderComponent {
       .catch(error => {
         console.error('Erro ao fazer logout:', error);
       });
-  }
-
-  /**
-   * Método para navegar para a tela inicial da instituição.
-   * Usa o Router.navigate e registra no console se a navegação foi bem-sucedida ou não.
-   */
-  goHome(): void {
-    this.router.navigate(['/institution/home'])
-      .then(success => console.log('Navegação realizada:', success))
-      .catch(error => console.error('Erro na navegação:', error));
   }
 }
