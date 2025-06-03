fa2e18d2e27447cba336757d4cd8f186defcb679
Luan Magri Tarran <luan.mtarran@gmail.com>
2025-06-03 10:54:05 -0300

    back button

---
+++ src/app/shared/buttons/back-button/back-button.component.ts
@@ -0,0 +1,32 @@
+import { Location } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-back-button',
+  imports: [],
+  templateUrl: './back-button.component.html',
+  styleUrl: './back-button.component.scss'
+})
+export class BackButtonComponent {
+
+  constructor(
+    /** Injeção do serviço de localização do Angular @type {Location} */
+    private location: Location
+  ) { }
+
+  /**
+   * Método para navegar de volta à página anterior
+   */
+  goBack(): void {
+    this.location.back();
+  }
+  
+  /**
+   * Método para verificar se é possível voltar na navegação
+   * @returns {boolean} Retorna true se for possível voltar, caso contrário false
+   */
+  canGoBack(): boolean {
+    const state = this.location.getState() as { previousUrl?: string };
+    return this.location.isCurrentPathEqualTo(state.previousUrl ?? '');
+  }
+}
