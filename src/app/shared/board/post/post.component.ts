cfcf35b9ccd3a7f7d160a05e87c607a0f748c67f
Marcos <marcos.c.junior2@gmail.com>
2025-06-04 22:00:06 -0300

    Dando push

--- src/app/shared/board/post/post.component.ts
+++ src/app/shared/board/post/post.component.ts
@@ -63,9 +63,20 @@ export class PostComponent implements OnInit {
    * @param {string} url - A URL a ser sanitizada
    * @returns {SafeResourceUrl} - A URL sanitizada
    */
-  sanitizeUrl(url: string): SafeResourceUrl {
-    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+sanitizeUrl(url: string): SafeResourceUrl {
+  // Extrai o ID do vídeo a partir de diferentes formatos
+  const videoIdMatch = url.match(/(?:v=|\/embed\/|\.be\/)([a-zA-Z0-9_-]{11})/);
+  const videoId = videoIdMatch ? videoIdMatch[1] : null;
+
+  if (videoId) {
+    const embedUrl = `https://www.youtube.com/embed/${videoId}`;
+    return this.sanitizer.bypassSecurityTrustResourceUrl(embedUrl);
   }
+
+  return this.sanitizer.bypassSecurityTrustResourceUrl('');
+}
+
+
 
   /** Fecha o modal de post */
   onClose() {
@@ -158,5 +169,17 @@ export class PostComponent implements OnInit {
         }
       });
   }
+
+
+  formatDate(dateString: Date | string): string {
+  const date = new Date(dateString);
+  const dia = String(date.getDate()).padStart(2, '0');
+  const mes = String(date.getMonth() + 1).padStart(2, '0'); // meses começam em 0
+  const ano = date.getFullYear();
+  const horas = String(date.getHours()).padStart(2, '0');
+  const minutos = String(date.getMinutes()).padStart(2, '0');
+
+  return `${dia}/${mes}/${ano} - ${horas}:${minutos}`;
+}
 
 }
