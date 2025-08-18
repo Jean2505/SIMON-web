1d713ca9ed2b4082021fe5c12b996ee69ea2def4
Jean2505 <jean.victor12357@gmail.com>
2025-08-18 20:36:35 -0300

    Configuração do botão de envio de requisição

--- src/app/professor/request-tutoring/request-modal/request-modal.component.ts
+++ src/app/professor/request-tutoring/request-modal/request-modal.component.ts
@@ -1,24 +1,53 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 import { Discipline } from '../../../models/discipline.model';
 
 @Component({
   selector: 'app-request-modal',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './request-modal.component.html',
   styleUrl: './request-modal.component.scss'
 })
-export class RequestModalComponent {
+export class RequestModalComponent implements OnInit{
 
   /** Objeto Discipline com dados da matéria. */
   @Input({ required: true }) discipline!: Discipline;
   
   @Output() cancel = new EventEmitter<boolean>();
 
+  requestQuantity: Number | undefined;
+
+  /**
+   * @param http - HttpClient para requisições HTTP.
+   */
+  constructor(
+    /** Serviço HttpClient para requisições HTTP @type {HttpClient} */
+    private http: HttpClient
+  ){}
+
+  ngOnInit(): void {
+    this.requestQuantity = this.discipline.monitors;
+  }
+
   cancelModal() {
     this.cancel.emit(false);
   }
 
-  sendRequest() {
-
+  sendRequest(): void {
+    this.http
+      .post('http://localhost:3000/sendMonitorRequest', {
+        id: this.discipline.id,
+        name: this.discipline.name,
+        course: this.discipline.course,
+        professor: this.discipline.professor,
+        monitors: this.discipline.monitors,
+        requestQuantity: this.requestQuantity
+      })
+      .subscribe({
+        next: (response: any) => {
+          console.log(response);
+        }
+      });
   }
 }
