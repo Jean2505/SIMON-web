512de3edd235dba4787b89469372b23a3a3b380c
Jean2505 <jean.victor12357@gmail.com>
2025-09-05 21:07:09 -0300

    Roteamento da notificação para a tela de candidatura

--- src/app/shared/notifications/notification/notification.component.ts
+++ src/app/shared/notifications/notification/notification.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-notification',
@@ -10,5 +11,19 @@ export class NotificationComponent {
 
   @Input({ required: true }) notification: any;
 
-  
+  /**
+   * Construtor do componente.
+   * @param router - Serviço Router para navegação programática.
+   */
+  constructor(private router: Router) {}
+
+  goEnlist(): void{
+    this.router
+      .navigate(['/student/enlist'])
+      .then((success) => {
+        console.log('Navegação realizada:', success);
+        console.clear(); // Limpa console após navegação bem-sucedida
+      })
+      .catch((error) => console.error('Erro na navegação:', error));
+  }
 }
