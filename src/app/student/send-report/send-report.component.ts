27c668d600ee109c0c486519eaf0617a7aac68fd
Jean <jean.victor12357@gmail.com>
2025-09-21 16:42:16 -0300

    Botão para acessar a página do relatório; Criação de componente de envio de relatório

---
+++ src/app/student/send-report/send-report.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { BackButtonComponent } from "../../shared/buttons/back-button/back-button.component";
+
+@Component({
+  selector: 'app-send-report',
+  imports: [BackButtonComponent],
+  templateUrl: './send-report.component.html',
+  styleUrl: './send-report.component.scss'
+})
+export class SendReportComponent {
+
+}
